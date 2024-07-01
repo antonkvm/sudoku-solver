959ec4471c5a2a1dbab56a136318048e4a93f200
Anton Kaevenheim <akaevenheim@gmail.com>
2024-07-01 19:11:40 +0200

    formatting and linting

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from sudoku import SudokuGrid
 import puzzles
+from sudoku import SudokuGrid
 
-if __name__ == "__main__":
-	sudoku = SudokuGrid(puzzles.SKIENA_HARD)
-	sudoku.pretty_print()
-	sudoku.solve()
-	sudoku.pretty_print()
+if __name__ == '__main__':
+    sudoku = SudokuGrid(puzzles.SKIENA_HARD)
+    sudoku.pretty_print()
+    sudoku.solve()
+    sudoku.pretty_print()
